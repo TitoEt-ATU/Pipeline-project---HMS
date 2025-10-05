d528453a89713cb47bd97b8e98829a83943f5567
TitoEt-ATU <L00203089@atu.ie>
2025-10-05 21:09:34 +0100

    Initialize database

--- src/app.py
+++ src/app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from config.settings import Config
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
 
     @app.route('/')
     def home():
